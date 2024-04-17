f3cae07a71f2ac47f5befe405089b75a00c1d127
mahnoor <mabbasi@xavor.com>
2024-04-17 11:33:28 +0500

    workshop for reactive examples

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -9,21 +9,61 @@ public class Exercise1 {
         // Print all numbers in the intNumbersStream stream
         // TODO: Write code here
 
+        StreamSources.intNumbersStream().forEach(
+                element -> System.out.println(element)
+        );
+        System.out.println("============");
         // Print numbers from intNumbersStream that are less than 5
         // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .filter(x -> x < 5)
+                .forEach(
+                        element -> System.out.println(element)
+                );
+        System.out.println("============");
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
         // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .filter(x -> x < 5)
+                .skip(1)
+                .limit(2)
+                .forEach(
+                        element -> System.out.println(element)
+                );
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
         // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .filter(x -> x > 5)
+                .findFirst()
+                .ifPresentOrElse(System.out::println, () -> System.out.println(-1));
+
+        Integer value = StreamSources.intNumbersStream()
+                .filter(x -> x > 5)
+                .findFirst()
+                .orElse(-1);
+        System.out.println(value);
+
 
         // Print first names of all users in userStream
         // TODO: Write code here
-
+        StreamSources.userStream()
+                .forEach(user -> System.out.println(user.getFirstName()));
+        StreamSources.userStream()
+                .map(user -> user.getFirstName())
+                .forEach(userName -> System.out.println(userName));
         // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
+
+        StreamSources.userStream()
+                .filter(user -> StreamSources.intNumbersStream().anyMatch(n -> n == user.getId()))
+                .forEach(userName -> System.out.println(userName));
+
+        StreamSources.intNumbersStream()
+                .flatMap(id -> StreamSources.userStream().filter(user -> user.getId() == id))
+                .map(user -> user.getFirstName())
+                .forEach(userName -> System.out.println(userName));
 
     }
 
--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.SignalType;
 
 import java.io.IOException;
 
@@ -12,14 +13,28 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .subscribe(x -> System.out.println(x),
+                        err -> System.out.println("error")); //error no bubble up
+
+        ReactiveSources.intNumbersFluxWithException()
+                .doOnError(err -> System.out.println(err.getMessage()))
+                .subscribe(x -> System.out.println(x));    //error will bubble up
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
-
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorContinue((err, item) -> System.out.println(err.getMessage()))
+                .subscribe(x -> System.out.println(x));
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorResume((err -> Flux.just(-1, -2)))
+                .doFinally(signal -> {
+                    if (signal == SignalType.ON_COMPLETE)
+                        System.out.println("fin");
+                    else System.out.println("error?");
+                })
+                .subscribe(x -> System.out.println(x));
 
         System.out.println("Press a key to end");
         System.in.read();
